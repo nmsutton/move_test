66c0b8121a64a7fcd07f84d0820e98632dd0d381
Nate Sutton <natemsutton@gmail.com>
2021-11-30 20:17:20 -0500

    redesigned code based on touretzky model

--- src/move_path.cpp
+++ src/move_path.cpp
@@ -11,135 +11,138 @@ struct G {
 	int pos[2] = {1,1};
 	int run_time = 23; // sim run time
 	char last_dir; // last dir command
+	double tau = .9; // time constant; TODO: add diff equ for more realistic one
+	double a_sym = 0.5; // alpha sym
+	double a_asym = -1.5; // alpha asym
 };
 
-void ext_input(char direction, double speed, double *gc_firing, G* g);
+void ext_input(char direction, double speed, double *gc_firing, double (*gc_weights)[100], G* g);
 
-void move_path(double *gc_firing, int t, G* g) {
+void move_path(double *gc_firing, double (*gc_weights)[100], int t, G* g) {
 	// movement path
 
 	double speed = g->speed;
 	if (t == 1) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 3) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 5) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 7) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 9) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 11) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 13) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 15) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 17) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 19) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 21) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 23) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 24) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 25) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 26) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 27) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 28) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 29) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 30) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 31) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 32) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 33) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 34) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 35) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 36) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 37) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 38) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 39) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 40) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 41) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 42) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 43) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 44) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 45) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 46) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 47) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 48) {
-		ext_input('l', speed, gc_firing, g);
+		ext_input('l', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 49) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 50) {
-		ext_input('u', speed, gc_firing, g);
+		ext_input('u', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 51) {
-		ext_input('d', speed, gc_firing, g);
+		ext_input('d', speed, gc_firing, gc_weights, g);
 	}
 	else if (t == 52) {
-		ext_input('r', speed, gc_firing, g);
+		ext_input('r', speed, gc_firing, gc_weights, g);
 	}
 }
