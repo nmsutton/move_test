1aa2ffdb8b7e9da805496ce1896f31de2d1eb15e
Nate Sutton <natemsutton@gmail.com>
2021-12-02 20:34:46 -0500

    added movie generater

--- src/movie_gen.m
+++ src/movie_gen.m
@@ -1,11 +1,12 @@
 % author: Nate Sutton 2021
 %
-% reference: https://www.mathworks.com/matlabcentral/answers/154659-how-to-create-animation-of-matlab-plotting-points-on-a-graph
+% references: 
+% https://www.mathworks.com/matlabcentral/answers/154659-how-to-create-animation-of-matlab-plotting-points-on-a-graph
+% https://www.mathworks.com/matlabcentral/answers/220094-how-do-i-make-a-video-of-a-graph-with-axes-and-axes-labels
+% https://www.mathworks.com/matlabcentral/answers/285058-how-to-keep-the-numbers-on-the-colorbar-scale-of-a-surface-plot-fixed
 
 clear all;
 clc;
-%x=[-3:0.1:3];
-%y=[-3:0.1:3];
 time=51; % time steps
 t=[0:0.1:(time*.1)];
 hFigure = figure;
@@ -22,26 +23,17 @@ allTheColorMaps = cell(numberOfFrames,1);
 allTheColorMaps(:) = {zeros(256, 3)};
 % Now combine these to make the array of structures.
 myMovie = struct('cdata', allTheFrames, 'colormap', allTheColorMaps);
-% Create a VideoWriter object to write the video out to a new, different file.
-%writerObj = VideoWriter('problem_3.avi');
-%open(writerObj);
+set(gcf, 'nextplot', 'replacechildren'); 
 % Need to change from the default renderer to zbuffer to get it to work right.
 % openGL doesn't work and Painters is way too slow.
-set(gcf, 'nextplot', 'replacechildren'); 
 set(gcf, 'renderer', 'zbuffer');
-% [x, y] = meshgrid(-3:.1:3,-3:.1:3);
 caxis manual;          % allow subsequent plots to use the same color limits
 for frameIndex = 1 : numberOfFrames
-  %disp(frameIndex)
   filename = strcat('../output/firing_t',int2str(frameIndex),'.csv');
   [test,delimiterOut]=importdata(filename);
-  %z = exp(-(x-t(frameIndex)).^2-(y-t(frameIndex)).^2);
-  %test=[1 2 3;4 5 6;7 8 9];
   cla reset;
   % Enlarge figure to full screen.
   %   set(gcf, 'Units', 'Normalized', 'Outerposition', [0, 0, 1, 1]);
-  %surf(x,y,z);
-  %imagesc(test);
   imagesc(test);
   %imresize(img,[343 434]);
   axis('tight')
@@ -54,36 +46,12 @@ for frameIndex = 1 : numberOfFrames
   %zlim([0, 1]);
   caption = sprintf('Grid cell firing amounts, t = %.0f', frameIndex);
   title(caption, 'FontSize', 15);
-  %drawnow;
-  %shading interp;
   thisFrame = getframe(gcf);
-  %lighting phong;
-  % Write this frame out to a new video file.
-  %size(thisFrame.cdata)
-  %writeVideo(writerObj, thisFrame);
   myMovie(frameIndex) = thisFrame;
 end
-%close(writerObj);
-%message = sprintf('Done creating movie\nDo you want to play it?');
-%button = questdlg(message, 'Continue?', 'Yes', 'No', 'Yes');
-%drawnow;  % Refresh screen to get rid of dialog box remnants.
 close(hFigure);
-%if strcmpi(button, 'No')
-%   return;
-%end
-%hFigure = figure;
-% Enlarge figure to full screen.
-% set(gcf, 'Units', 'Normalized', 'Outerposition', [0, 0, 1, 1]);
-%title('Playing the movie we created', 'FontSize', 15);
-% Get rid of extra set of axes that it makes for some reason.
-%axis off;
-% Play the movie.
-%movie(myMovie);
-%uiwait(helpdlg('Done with demo!'));
-%close(hFigure);
-myMovie(1) = []; % remove first frame causing issues
-v = VideoWriter('firing.avi');
+myMovie(1) = []; % remove first frame causing issues due to wrong size
+v = VideoWriter('firing.avi'); % Create a VideoWriter object to write the video out to a new, different file.
 open(v)
-
-writeVideo(v,myMovie)
+writeVideo(v,myMovie) % Write the movie object to a new video file.
 close(v)
