7045dd78ee2c11843567ebef8f422cd571fd8c82
Nate Sutton <natemsutton@gmail.com>
2021-12-04 13:49:18 -0500

    layer size increased

--- src/move_path.cpp
+++ src/move_path.cpp
@@ -11,7 +11,10 @@ struct G {
 	int pos[2] = {1,1};
 	int run_time = 23; // sim run time
 	char last_dir; // last dir command
-	double dist_thresh = 3.0; // distance threshold for only local connections
+	double dist_thresh = 5.0; // distance threshold for only local connections
+	int layer_x = 26;
+	int layer_y = 26;
+	int layer_size = layer_x * layer_y;
 };
 
 void ext_input(char direction, double speed, double *gc_firing, G* g);
@@ -142,5 +145,212 @@ void move_path(double *gc_firing, int t, G* g) {
 	}
 	else if (t == 52) {
 		ext_input('r', speed, gc_firing, g);
+	}
+	else if (t == 53) {
+		ext_input('u', speed, gc_firing, g);
+	}
+	else if (t == 54) {
+		ext_input('r', speed, gc_firing, g);
+	}
+	else if (t == 55) {
+		ext_input('d', speed, gc_firing, g);
+	}
+	else if (t == 56) {
+		ext_input('r', speed, gc_firing, g);
+	}
+	else if (t == 57) {
+		ext_input('r', speed, gc_firing, g);
+	}
+	else if (t == 58) {
+		ext_input('l', speed, gc_firing, g);
+	}
+	else if (t == 59) {
+		ext_input('l', speed, gc_firing, g);
+	}
+	else if (t == 60) {
+		ext_input('d', speed, gc_firing, g);
+	}
+	else if (t == 61) {
+		ext_input('l', speed, gc_firing, g);
+	}
+	else if (t == 62) {
+		ext_input('r', speed, gc_firing, g);
+	}
+	else if (t == 63) {
+		ext_input('u', speed, gc_firing, g);
+	}
+	else if (t == 64) {
+		ext_input('d', speed, gc_firing, g);
+	}
+	else if (t == 65) {
+		ext_input('r', speed, gc_firing, g);
+	}
+}
+
+void move_path2(double *gc_firing, int t, G* g) {
+	// movement path
+
+	double speed = g->speed;
+	if (t == 1) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 3) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 5) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 7) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 9) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 11) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 13) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 15) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 17) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 19) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 21) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 23) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 24) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 25) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 26) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 27) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 28) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 29) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 30) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 31) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 32) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 33) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 34) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 35) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 36) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 37) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 38) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 39) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 40) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 41) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 42) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 43) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 44) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 45) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 46) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 47) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 48) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 49) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 50) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 51) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 52) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 53) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 54) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 55) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 56) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 57) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 58) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 59) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 60) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 61) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 62) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 63) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 64) {
+		ext_input('n', speed, gc_firing, g);
+	}
+	else if (t == 65) {
+		ext_input('n', speed, gc_firing, g);
 	}
 }
--- src/move_test.cpp
+++ src/move_test.cpp
@@ -34,19 +34,36 @@ string int_to_string(int x)
 }
 
 char get_pd(int x, int y) {
-	char pd[100] = { 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'd', 'r', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u', 'l', 'u' }; 
+	char pd;
 
-	int gc_ind = (y * 10) + x;
+	if (y % 2 == 0) {
+		if (x % 2 == 0) {
+			pd = 'd';
+		}
+		else {
+			pd = 'r';
+		}
+	}
+	else {
+		if (x % 2 == 0) {
+			pd = 'l';
+		}
+		else {
+			pd = 'u';
+		}		
+	}
 
-	return pd[gc_ind];
+	return pd;
 }
 
-void write_firing(int layer_x, int layer_y, double *gc_firing, int t, G* g) {
+void write_firing(double *gc_firing, int t, G* g) {
 	ofstream output_file;
 	string filename = "output/firing_t" + int_to_string(t) + ".csv";
 	output_file.open(filename);
 
-	int layer_size = layer_x * layer_y;
+	int layer_x = g->layer_x;
+	int layer_y = g->layer_y;
+	int layer_size = g->layer_size;
 	int gc_ind = 0; // grid cell index
 	double temp;
 
@@ -70,8 +87,10 @@ void write_firing(int layer_x, int layer_y, double *gc_firing, int t, G* g) {
   output_file.close();
 }
 
-void print_firing(int layer_x, int layer_y, double *gc_firing, int t, G* g) {
-	int layer_size = layer_x * layer_y;
+void print_firing(double *gc_firing, int t, G* g) {
+	int layer_x = g->layer_x;
+	int layer_y = g->layer_y;
+	int layer_size = g->layer_size;
 	int gc_ind = 0; // grid cell index
 	double temp;
 
@@ -83,24 +102,24 @@ void print_firing(int layer_x, int layer_y, double *gc_firing, int t, G* g) {
 			for (int j = 0; j < layer_y; j++) {
 				gc_ind = (i * layer_x) + j;
 
-				printf("[%f]",gc_firing[gc_ind]);
+				printf("[%.1f]",gc_firing[gc_ind]);
 
-				if (g->pos[0] == j && g->pos[1] == i) {
+				/*if (g->pos[0] == j && g->pos[1] == i) {
 					printf("(%c)",g->last_dir);
-				}
+				}*/
 
-				printf("\t");
+				printf(" ");
 			}
 			cout << "\n";
 		}
 	}
 }
 
-double get_distance(int x1, int y1, int x2, int y2, char pd) {
+double get_distance(int x1, int y1, int x2, int y2, char pd, G *g) {
 	// d = sqrt((e_x - i_x - o_x)^2+(e_y - i_y - o_y)^2)
 	int x2_x1 = (x2 - x1);
 	int y2_y1 = (y2 - y1);
-	int half_point = 10 / 2; // layer length divided by 2
+	int half_point = g->layer_x / 2; // layer length divided by 2
 
 	// preferred direction bias
 	if (pd == 'u') {
@@ -119,10 +138,10 @@ double get_distance(int x1, int y1, int x2, int y2, char pd) {
 	// torus wrap around
 	if (abs(x2_x1) >= half_point) {
 		// distance wraps half way around
-		x2_x1 = (10 - abs(x2_x1));
+		x2_x1 = (g->layer_x - abs(x2_x1));
 	}
 	if (abs(y2_y1) >= half_point) {
-		y2_y1 = (10 - abs(y2_y1));
+		y2_y1 = (g->layer_y - abs(y2_y1));
 	}
 
 	double d = sqrt(pow(x2_x1,2)+pow(y2_y1,2));
@@ -138,9 +157,9 @@ void init_firing(double *gc_firing, int layer_size) {
  	double w3 = 1.473271;
  	double scaling_factor = 0.5;
 	for (int i = 0; i < layer_size; i++) {
-		gc_firing[i] = 0.3;
+		gc_firing[i] = 1.0;
 	}
-	gc_firing[11] = w1;
+	/*gc_firing[11] = w1;
 	gc_firing[1] = w2;
 	gc_firing[10] = w2;
 	gc_firing[12] = w2;
@@ -148,7 +167,7 @@ void init_firing(double *gc_firing, int layer_size) {
 	gc_firing[0] = w3;
 	gc_firing[20] = w3;
 	gc_firing[2] = w3;
-	gc_firing[22] = w3;
+	gc_firing[22] = w3;*/
 }
 
 void set_pos(G* g, char direction) {
@@ -176,8 +195,8 @@ void ext_input(char direction, double speed, double *gc_firing, G* g) {
 		mexican hat: (2/(sqrt(pow((3*sigma*PI),(1/4))))*(pow(1-pow(x/sigma,2),(exp(pow(-1*x,2)/pow(2*sigma,2))))))
 	*/	
 
-	double new_gc_firing[100];
-	for (int i = 0; i < 100; i++) { // initialize array
+	double new_gc_firing[g->layer_size];
+	for (int i = 0; i < g->layer_size; i++) { // initialize array
 		new_gc_firing[i] = 0.000001;
 	}
 	int pd_i, gc_i;
@@ -187,7 +206,7 @@ void ext_input(char direction, double speed, double *gc_firing, G* g) {
 	g->s_1 = 1.8*.404318655; // sigma_1. Note: specific value used for equalibrium of weights over time.
 	g->s_2 = 1.8; //g->s_1;
 	g->s_3 = 1.8; //g->s_1;
-	g->run_time = 52;
+	g->run_time = 3;
 
 	double y_inter = g->y_inter;
 	double s_1 = g->s_1;
@@ -196,22 +215,26 @@ void ext_input(char direction, double speed, double *gc_firing, G* g) {
 
 	set_pos(g, direction);
 
-	for (int pdy = 0; pdy < 10; pdy++) {
-		for (int pdx = 0; pdx < 10; pdx++) {
+	for (int pdy = 0; pdy < g->layer_y; pdy++) {
+		for (int pdx = 0; pdx < g->layer_x; pdx++) {
 			//if (direction == get_pd(pdx, pdy) && pdx == 1 && pdy == 1) {
-			if (direction == get_pd(pdx, pdy)) {
-				pd_i = (pdy * 10) + pdx;
-				for (int gcy = 0; gcy < 10; gcy++) {
-					for (int gcx = 0; gcx < 10; gcx++) {			
-						gc_i = (gcy * 10) + gcx;
+			//if (direction == get_pd(pdx, pdy)) {
+			//if (true) {
+			get_pd(pdx, pdy);
+			if (pdx == 2 && pdy == 2) {
+				pd_i = (pdy * g->layer_x) + pdx;
+				for (int gcy = 0; gcy < g->layer_y; gcy++) {
+					for (int gcx = 0; gcx < g->layer_x; gcx++) {			
+						gc_i = (gcy * g->layer_x) + gcx;
 
-						d = get_distance(pdx, pdy, gcx, gcy, direction);
+						d = get_distance(pdx, pdy, gcx, gcy, direction, g);
 
 						if (d < g->dist_thresh) { 
 
 							mex_hat = (2/(sqrt(3*s_1*pow(PI,1/4))))*(1-pow(d/s_2,2))*(exp(pow(-1*d,2)/pow(2*s_3,2)));
 
 							new_firing = y_inter + gc_firing[pd_i] * mex_hat;
+							new_firing = d;//mex_hat;
 
 							if (new_firing < 0) {
 								new_firing = 0.00001; // avoid negative
@@ -225,26 +248,23 @@ void ext_input(char direction, double speed, double *gc_firing, G* g) {
 		}
 	}
 
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < g->layer_size; i++) {
 		gc_firing[i] = new_gc_firing[i];
 	}
 }
 
 int main() {
-	int layer_x = 10;
-	int layer_y = 10;
-	int layer_size = layer_x * layer_y;
-	double gc_firing[layer_size];
-	struct G g;
+	struct G g;	
+	double gc_firing[g.layer_size];
 	
-	init_firing(gc_firing, layer_size);
+	init_firing(gc_firing, g.layer_size);
 
 	for (int t = 0; t <= g.run_time; t++) {
-		move_path(gc_firing, t, &g);
+		move_path2(gc_firing, t, &g);
 
-		print_firing(layer_x, layer_y, gc_firing, t, &g);
+		print_firing(gc_firing, t, &g);
 
-		write_firing(layer_x, layer_y, gc_firing, t, &g);		
+		//write_firing(gc_firing, t, &g);		
 	}
 
 	return 0;
